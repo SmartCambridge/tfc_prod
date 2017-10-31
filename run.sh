34c696d59684fd5a0867edfe63ee506b0bf84567
Ian Lewis <ijl20@cam.ac.uk>
2017-10-31 16:53:28 +0000

    Updated run.sh to include SIRIVM and GTFS-FEEDMAKER services

--- run.sh
+++ run.sh
@@ -54,26 +54,41 @@
 # CONSOLE
 nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.console.A" -cluster >/dev/null 2>>/var/log/tfc_prod/console.A.err & disown
 
-# FEEDHANDLER FOR VIX GTFS DATA
-nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.feedhandler.vix" -cluster >/dev/null 2>>/var/log/tfc_prod/feedhandler.vix.err & disown
-
 # DATASERVER TO PROVIDE DATA API FOR TFC_WEB
 nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.dataserver.vix" -cluster >/dev/null 2>>/var/log/tfc_prod/dataserver.vix.err & disown
+
+# #############################################################################################
+# ################  PARK_LOCAL_RSS FEEDMAKER  #################################################
+# #############################################################################################
 
 # FEEDMAKER TO SCRAPE CAR PARK WEB SITES
 nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.feedmaker.park_local_rss" -cluster >/dev/null 2>>/var/log/tfc_prod/feedmaker.park_local_rss.err & disown
 
-# MSGFILER TO STORE ZONE TRANSIT MESSAGES
-nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.msgfiler.vix.zone_cambridge" -cluster >/dev/null 2>>/var/log/tfc_prod/msgfiler.vix.zone_cambridge.err & disown
-
-# MSGFILER TO STORE JSON VIX GTFS MESSAGES (i.e. from feedhandler.vix, parsed from Google Protobuf)
-nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.msgfiler.vix.feed_json" -cluster >/dev/null 2>>/var/log/tfc_prod/msgfiler.vix.feed_json.err & disown
-
 # MSGFILER TO STORE MESSAGES FROM CAR PARKS FEEDMAKER (i.e. from feedmaker.park_local_rss)
 nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.msgfiler.cam.to_json" -cluster >/dev/null 2>>/var/log/tfc_prod/msgfiler.cam.to_json.err & disown
 
-# ZONEMANAGER (launches all the Cambridge zone verticles)
+# #############################################################################################
+# ################  VIX GTFS FEEDHANDLER  #####################################################
+# #############################################################################################
+
+# FEEDHANDLER FOR VIX GTFS DATA
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.feedhandler.vix" -cluster >/dev/null 2>>/var/log/tfc_prod/feedhandler.vix.err & disown
+
+# MSGFILER TO STORE VIX ZONE TRANSIT MESSAGES
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.msgfiler.vix.zone_cambridge" -cluster >/dev/null 2>>/var/log/tfc_prod/msgfiler.vix.zone_cambridge.err & disown
+
+# MSGFILER TO STORE VIX JSON GTFS MESSAGES (i.e. from feedhandler.vix, parsed from Google Protobuf)
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.msgfiler.vix.feed_json" -cluster >/dev/null 2>>/var/log/tfc_prod/msgfiler.vix.feed_json.err & disown
+
+# ZONEMANAGER VIX (launches all the Cambridge zone verticles)
 nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.zonemanager.cambridge.vix" -cluster >/dev/null 2>>/var/log/tfc_prod/zonemanager.cambridge.vix.err & disown
+
+# #############################################################################################
+# ################  EVERYNET FEED HANDLER  ####################################################
+# #############################################################################################
+
+# EVERYNETFEED (receives http PUSH sensor data messages from EveryNet)
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.everynet_feed.A" -cluster >/dev/null 2>>/var/log/tfc_prod/everynet_feed.A.err & disown
 
 # MSGROUTER (forwards EveryNet messages to onward destinations)
 nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.msgrouter.A" -cluster >/dev/null 2>>/var/log/tfc_prod/msgrouter.A.err & disown
@@ -81,7 +96,34 @@ nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_serve
 # HTTPMSG (command API for tfc_web)
 nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.httpmsg.A" -cluster >/dev/null 2>>/var/log/tfc_prod/httpmsg.A.err & disown
 
-# EVERYNETFEED (receives http PUSH sensor data messages from EveryNet)
-nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.everynet_feed.A" -cluster >/dev/null 2>>/var/log/tfc_prod/everynet_feed.A.err & disown
+# #############################################################################################
+# ################   VIX GTFS FEEDMAKER  ######################################################
+# #############################################################################################
 
+# VIX2 FEEDMAKER FOR GTFS VIX DATA
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.feedmaker.vix2" -cluster >/dev/null 2>>/var/log/tfc_prod/feedmaker.vix2.err & disown
+
+# VIX2 ZONEMANAGER FOR GTFS VIX DATA
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.zonemanager.vix2" -cluster >/dev/null 2>>/var/log/tfc_prod/zonemanager.vix2.err & disown
+
+# VIX2 MSGFILER FOR GTFS VIX DATA AND ZONE TRANSITS
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.msgfiler.vix2" -cluster >/dev/null 2>>/var/log/tfc_prod/msgfiler.vix2.err & disown
+
+# #############################################################################################
+# ################   SIRIVM CLOUDAMBER FEEDMAKER  #############################################
+# #############################################################################################
+
+# SIRIVM FEEDMAKER FOR CLOUDAMBER SIRIVM DATA
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.feedmaker.cloudamber.sirivm" -cluster >/dev/null 2>>/var/log/tfc_prod/feedmaker.cloudamber.sirivm.err & disown
+
+# SIRIVM ZONEMANAGER FOR CLOUDAMBER SIRIVM DATA
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.zonemanager.cloudamber.sirivm" -cluster >/dev/null 2>>/var/log/tfc_prod/zonemanager.cloudamber.sirivm.err & disown
+
+# SIRIVM MSGFILER FOR CLOUDAMBER SIRIVM DATA
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.msgfiler.cloudamber.sirivm" -cluster >/dev/null 2>>/var/log/tfc_prod/msgfiler.cloudamber.sirivm.err & disown
+
+# SIRIVM MSGROUTER (tfc-app2 only)
+if [ "$HOSTNAME" = tfc-app2 ]; then
+nohup java -cp tfc$1.jar io.vertx.core.Launcher run "service:uk.ac.cam.tfc_server.msgrouter.cloudamber.sirivm" -cluster >/dev/null 2>>/var/log/tfc_prod/msgrouter.cloudamber.sirivm.err & disown
+fi
 
