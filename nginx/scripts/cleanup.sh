0d496f66196aa21d958ceb4f520827135bfaa52b
Jon Warbrick <jw35@cam.ac.uk>
2018-12-07 09:19:35 +0000

    Restart nginx from deploy hook, not cleanup
    The certbot renewal cleanup hook is run after each domain validation,
    but probably before the certificate is actually issued. So it's the wrong
    place to reload nginx to pick up a new certificate. The right place to do
    this is ina  deploy hook, and the easiest way to specify a deploy hook
    is by putting a script in /etc/letsencrypt/renewal-hooks/deploy/
    
    Hopefully fixes #21, but only time will tell.

--- nginx/scripts/cleanup.sh
+++ nginx/scripts/cleanup.sh
@@ -8,10 +8,5 @@ key="/root/acme-challenge-keys/acme-challenge-key"
 ( echo "cleanup"
   echo "${CERTBOT_DOMAIN}"
   echo "${CERTBOT_TOKEN}"
-  echo "${CERTBOT_VALIATION}"
+  echo "${CERTBOT_VALIDATION}"
 ) | ssh -i "${key}" acme-challenge@"${CERTBOT_DOMAIN}" acme-challenge
-
-logger -p daemon.error -t cleanup.sh "Restarting nginx for ${CERTBOT_DOMAIN} certificate change"
-ls -lR /etc/letsencrypt/live/ | logger -p daemon.error -t cleanup.sh
-service nginx reload
-ls -lR /etc/letsencrypt/live/ | logger -p daemon.error -t cleanup.sh
