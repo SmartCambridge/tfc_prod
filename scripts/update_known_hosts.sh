5e2a5446268c50a542d5b4ac59fa1936eb7d6ca8
ijl20 <ijl20@cam.ac.uk>
2019-03-04 15:04:00 +0000

    scripts/update_known_hosts.sh better usage comments

--- scripts/update_known_hosts.sh
+++ scripts/update_known_hosts.sh
@@ -2,8 +2,17 @@
 
 # update_known_hosts.sh
 #
-# Will collect host ssh keys from tfc-app[1-5] and add those to /etc/ssh/ssh_known_hosts
+# Will collect host ssh keys from tfc-app[1-5] and write to STDOUT correct entries for
+# /etc/ssh/ssh_known_hosts
 # A smartcambridge.org entry will be duplicated from each tfc-app[1-5] host key entry in ssh_known_host
+#
+# This script can most simply be used with:
+# scripts/update_known_hosts.sh >ssh_known_hosts
+# sudo mv ssh_known_hosts /etc/ssh/
+#
+# Alternatively (if you have existing entries you want to keep)
+# delete bad entries from /etc/ssh/ssh_known_hosts and append new records from above.
+#
 
 for n in 1 2 3 4 5;
 do
@@ -19,3 +28,4 @@ do
   echo $entry
   echo ${entry/$name/smartcambridge.org}
 done
+
